435f4c39b4ab4c21dac2d315956b290e966e63f3
diarmuidmalanaphy <d.malanaphy@lancaster.ac.uk>
2023-12-17 01:18:40 +0000

    fixed a bunch and added a more descriptive readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Thermal-detection
 Thermal detection in preparation for a separate project
+
+The main conceit of this project is to detect humans using a thermal camera without using any deep-learning techniques. This is due to the fact that this will later be built on to create a remote sensing drone, due to the nature of drones computation will be limited, and the computation will have to be done in real time.
+
+
+The data used for this project comes from 
+http://vcipl-okstate.org/pbvs/bench/Data/03/download.html
+
+The results from this project are promising, it was completed over the course of a day and a half, and i got decent detection working using mostly filters. 
+
+I had issues tracking the centre of mass of humans, as I was using the changes between photos.
+
+Next time I'll try to use a light neural network to detect the presence of humans
+
+
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import numpy as np
 
 from processData import increaseContrast
 from rectangle_modifiers import expand_and_group_rectangles, getSnapshotOfRectangles
-from filter_detectors import detect_moving_Objects, detect_stationary_figure
-import pytorch
+from filter_detectors import detect_moving_Objects, detect_stationary_figure, detect_stationary_objects
+
 
 #from processData import
 directory = "./5a"
@@ -54,41 +54,9 @@ for count, filename in enumerate(contents):
         ''' 4 '''
         stationary_rectangles = []
         if prev_rects is not None:
-            skip_counter = 0
+            stationary_rectangles = detect_stationary_objects(prev_rects,enhanced_frame) 
             # non_overlapping_rects = filter_non_overlapping_rectangles(prev_rects, grouped_rectangles)
-            snapshots = getSnapshotOfRectangles(prev_rects,enhanced_frame,with_rectangles=True)
-            for snapshot,rect in snapshots:
-                
-                if detect_stationary_figure(snapshot):
-                    
-                    stationary_rectangles.append(rect)
-                
-                    
-                    
-
-                    # Useful for creating a dataset, however it's not very good because it will only select things 
-                    # that my filters will detect.
-                    '''
-                    scale_factor = 20
-
-                    # Calculating the new size
-                    new_width = int(width * scale_factor)
-                    new_height = int(height * scale_factor)
-
-                    # Resize the image
-                    resized_snapshot = cv2.resize(snapshot, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
-
-                    # Display the resized image
-                    cv2.imshow('nonOverlapper', resized_snapshot)
-                    key = cv2.waitKey(10000)
-
-                    if key & 0xFF == ord('p'):
-                        # If 'p' is pressed, save the snapshot
-                        saved_filepath = save_snapshot_if_confirmed(snapshot)
-                    elif key & 0xFF == ord('q'):
-                        # If 'q' is pressed, break the loop to go to the next one
-                        skip_counter = 5
-                    '''
+            
         '''5'''
         human_rectangles = moving_rectangles + stationary_rectangles
             
@@ -111,9 +79,9 @@ for count, filename in enumerate(contents):
         # Display the original frame with rectangles
         '''7'''
         cv2.imshow('Detected Figures',  colour_frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-        cv2.groupRectangles(human_rectangles, 1, 0.1)
+        
         prev_rects = human_rectangles
     # Update the previous frame for the next iteration
     prev_frame = enhanced_frame
